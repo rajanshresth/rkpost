21dfa03756db13e9b862cd37555f8226467efa72
Rajan <shrestharajan726@gmail.com>
2023-05-29 17:39:38 +0545

    api build problem fix

--- app/todo/api/route.ts
+++ app/todo/api/route.ts
@@ -1,5 +1,4 @@
 import prisma from "@/prisma/prisma";
-import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
 export interface Post {
@@ -11,8 +10,9 @@ export interface Post {
   updatedAt: Date;
 }
 
-export async function POST(req: NextApiRequest) {
-  const { title, content } = typeof req.body=="string" ? JSON.parse(req.body) : req.body;
+export async function POST(req: Request) {
+  const { title, content } =
+    typeof req.body == "string" ? JSON.parse(req.body) : req.body;
   try {
     // we can access db records with prisma functions
     const res: Post = await prisma.post.create({
